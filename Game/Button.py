1c91bd04a08c6c5c098f73c7174d4461dde1d56b
Daan Boer <daanboer95@hotmail.com>
2016-01-25 14:13:21 +0100

    next

--- Game/Button.py
+++ Game/Button.py
@@ -8,7 +8,6 @@ class button:
         pygame.draw.rect(gameDisplay, black,(x-5,y-5,w+10,h+10))
         if x+w > mouse[0] > x and y+h > mouse[1] > y:
             pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
-
             if click[0] == 1 and action != None:
                 action()
         else:
